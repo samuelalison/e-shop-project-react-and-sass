e52c13d0619a96b0a3652608a2127544caea9ce7
samuelalison <samuelalison13@gmail.com>
2023-01-09 05:39:07 -0800

    'mobile-responsive'

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,27 @@
-import React from 'react'
-import { SearchOutlined, ShoppingBasket } from '@mui/icons-material'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
+import { SearchOutlined, ShoppingBasket } from '@mui/icons-material'
+// import GridViewIcon from '@mui/icons-material/GridView'
+import CloseIcon from '@mui/icons-material/Close'
+import MenuIcon from '@mui/icons-material/Menu'
 import '../styles/Header.scss'
 
 
 
 export const Header = () => {
+  const [sidebar, setSidebar] = useState(false)
+
+  // window.addEventListener("scroll", function () {
+  //   const header = document.querySelector('.header')
+  //   header.classList.toggle("active", window.screenY > 200)
+  // })
   return (
     <div className='header'>
      <nav>
-       <ul className='ul-items hide-for-mobile'>
+       <ul className={sidebar ? "nav-links-sidebar" : "hide-for-mobile"}  onClick={() => setSidebar(false)}>
         <li className='li-items'>
             <Link to='/women'>Women</Link>
-            <Link to='/men'>Men</Link>
+            <Link to='/men'>Men</Link> 
             <Link to='/denim'>Denim</Link>
             <Link to='/shop' className='orange'>The Gift Shop</Link>
             <Link to='/about'>About</Link>
@@ -24,7 +33,7 @@ export const Header = () => {
 
        <div className='right hide-for-mobile'>
         <div className="search">
-            <SearchOutlined className='search-icon '/>
+            <SearchOutlined className='search-icon'/>
             <button className='sch-btn'>Search</button>
         </div>
         
@@ -42,11 +51,8 @@ export const Header = () => {
         
         </div>
 
-        <Link to='#' className='hamburger hide-for-desktop'>
-            <span></span>
-            <span></span>
-            <span></span>
-          </Link>
+      
+          <button className='navbar-items-icons hide-for-desktop' onClick={() => setSidebar(!sidebar)}>{sidebar ?<CloseIcon/> : <MenuIcon/>}</button>
     </nav>  
     </div>
   )
